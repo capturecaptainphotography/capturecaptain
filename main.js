d844a4f0997104d761366bca19b0b75637f6a909
Nani500411 <82075407+Nani500411@users.noreply.github.com>
2024-03-11 06:40:51 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,64 @@
+
+ // Function to change contact details
+ function changeContactDetails() {
+    const newDetails = [                      //here you change contact datails
+        { iconClass: 'fas fa-home', content: 'hi New Street, New City, New Country' },
+        { iconClass: 'fas fa-phone', content: ' 987-654-3210' },
+        { iconClass: 'fas fa-envelope', content: 'example@example.com' }
+    ];
+
+    const contactInfoItems = document.querySelectorAll('.pc-contact-info-item');
+
+    contactInfoItems.forEach((item, index) => {
+        const iconElement = item.querySelector('.pc-contact-info-icon i');
+        const contentElement = item.querySelector('.pc-contact-info-content p');
+
+        if (iconElement && contentElement) {
+            iconElement.className = newDetails[index].iconClass;
+            contentElement.textContent = newDetails[index].content;
+        }
+    });
+}
+
+// Call the function to change contact details
+changeContactDetails();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     // Check if the device supports hover
     const isHoverSupported = window.matchMedia('(hover: hover)').matches;
